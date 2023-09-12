e1f88d7a0da4fcee2a2cee7109292fe49ebfb933
RaceFPV <racefpv@outlook.com>
2023-09-11 23:20:04 -0400

    add esp32 placeholder in src

---
+++ src/esp32_node/README.md
@@ -0,0 +1,3 @@
+# Receiver Node Firmware For ESP32 based boards
+
+The `src/node` directory contains the source code for the RotorHazard nodes.
---
+++ src/esp32_node/esp32_node.ino
@@ -0,0 +1,4 @@
+//This '.ino' file is a stub to support building via the original
+// Arduino IDE.  The real code is in the '.cpp' and '.h' files.
+//
+//The NODE_NUMBER definition is now in the 'config.h' file.
